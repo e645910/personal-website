5050f02fba945bc7e5aea7680fd9aad68a25e59f
rwightman <rick.wightman@q.com>
2015-09-10 20:49:28 -0600

    update css

--- public/js/aboutMe.js
+++ public/js/aboutMe.js
@@ -2,16 +2,10 @@ angular.module('myPersonalWeb')
 .controller('aboutMeCtrl', function($scope, $location, $window){
 	$scope.isCurrentPath = function (path) {
      return $location.path() == path;
-    }
-    // $scope.redirectToGoogle = function(){
-    //         $window.open('https://wildernessquiz.herokuapp.com/#/', '_blank');
-    //     };
-// function openInNewTab() {
-//   $scope.open = window.open('https://wildernessquiz.herokuapp.com/#/','_blank');
-// }
-
-$scope.openTab = function() {
+    };
+    
+	$scope.openTab = function() {
     $scope.url = 'https://wildernessquiz.herokuapp.com/#/';
-}
-
+    $scope.url1 = 'https://github.com/mPemba/groupProject/tree/master/api';
+	};
 });
